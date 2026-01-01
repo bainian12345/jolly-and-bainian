c9806ffe3afce2c0003131e13404c6b34c3a0472
Bainian Liu <bainian12345@hotmail.com>
2026-01-01 13:13:05 -0500

    serve frontend locally

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,19 @@ import Divider from "./Divider";
 import './App.css'
 import RSVPForm from './RsvpForm';
 
+import image1 from './assets/image1.JPG';
+import image2 from './assets/image2.JPG';
+import image3 from './assets/image3.JPG';
+import image4 from './assets/image4.JPG';
+import image5 from './assets/image5.JPG';
+import image6 from './assets/image6.JPG';
+import image7 from './assets/image7.JPG';
+import image8 from './assets/image8.JPG';
+import image9 from './assets/image9.JPG';
+import image10 from './assets/image10.JPG';
 import ImageCarousel from './ImageCarousel';
 
-const images = [
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image1.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image2.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image3.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image4.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image5.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image6.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image7.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image8.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image9.JPG',
-    'https://jolly-and-bainian-bucket.s3.us-east-1.amazonaws.com/image10.JPG',
-  ];
+const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10];
 
 function App() {
   const [showForm, setShowForm] = useState(false);
