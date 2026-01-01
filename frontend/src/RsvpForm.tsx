380f49143d17ce360c7271b25b318c9e4ce871ca
Bainian Liu <bainian12345@hotmail.com>
2026-01-01 17:47:36 -0500

    add inputs for plusone

--- frontend/src/RsvpForm.tsx
+++ frontend/src/RsvpForm.tsx
@@ -1,79 +1,96 @@
 import { useState } from "react";
 import "./RsvpForm.css";
+import addButton from "./assets/add-button.png";
+import minusButton from "./assets/minus-button.png";
+
+interface Guest {
+  firstName: string;
+  lastName: string;
+  email: string;
+  meal: string;
+}
+
 export default function RSVPForm() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    meal: "",
-  });
+  const [guest, setGuest] = useState<Guest>(
+    { firstName: "", lastName: "", email: "", meal: "" },
+  );
+  const [plusOne, setPlusOne] = useState<Guest>({ firstName: "", lastName: "", email: "", meal: "" });
+  const [hasPlusOne, setHasPlusOne] = useState<boolean>(false);
 
-  const mealOptions = ["Chicken", "Beef", "Vegetarian", "Vegan"];
+  const mealOptions = ["Braised Beef Short Ribs", "Coal Roasted Salmon", "Red Thai Coconut Curry (Vegetarian)"];
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    type: "guest" | "plusOne"
+  ) => {
     const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
+    
+    if (type === "guest") {
+      setGuest((prev) => ({ ...prev, [name]: value }));
+    } else {
+      setPlusOne((prev) => prev && { ...prev, [name]: value });
+    }
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Form submitted:", formData);
-    alert(`Thank you, ${formData.firstName}!`);
+    console.log(`Form submitted for ${guest.firstName} ${plusOne ? `and ${plusOne.firstName}` : ""}`);
+    alert(`Thank you, ${guest.firstName}!`);
+  };
+
+  const togglePlusOne = () => {
+    setHasPlusOne((prev) => !prev);
+  };
+
+  const getFormFields = (guest: Guest, onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void) => {
+    return (
+      <>
+      <div className="form-field">
+        <label>First Name<span className="required">*</span></label>
+        <input name="firstName" value={guest.firstName} onChange={onChange} required />
+      </div>
+
+      <div className="form-field">
+        <label>Last Name<span className="required">*</span></label>
+        <input name="lastName" value={guest.lastName} onChange={onChange} required />
+      </div>
+
+      <div className="form-field">
+        <label>Email</label>
+        <input name="email" value={guest.email} onChange={onChange} />
+      </div>
+
+      <div className="form-field">
+        <label>Meal Option<span className="required">*</span></label>
+        <select name="meal" value={guest.meal} onChange={onChange} required>
+          <option value="">Select a meal</option>
+          {mealOptions.map((meal) => (
+            <option key={meal} value={meal}>{meal}</option>
+          ))}
+        </select>
+      </div>
+    </>
+    );
   };
 
   return (
     <form onSubmit={handleSubmit} className="rsvp-form">
-      <div className="form-field">
-        <label htmlFor="firstName">First Name<span className="required">*</span></label>
-        <input
-          id="firstName"
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
-      </div>
+      <div className="form-container">
+        <div className="form-fields">
+          {/* <div className="guest-header">Guest Details</div> */}
+          {getFormFields(guest, (e) => handleChange(e, "guest"))}
+        </div>
 
-      <div className="form-field">
-        <label htmlFor="lastName">Last Name<span className="required">*</span></label>
-        <input
-          id="lastName"
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
-      </div>
+        <div className="toggle-plus-one-button" onClick={() => togglePlusOne()}>
+          <img src={hasPlusOne ? minusButton : addButton} alt="Add a +1" />
+          {hasPlusOne ? "Remove Plus One" : "Add Plus One"}
+        </div>
 
-      <div className="form-field">
-        <label htmlFor="email">Email</label>
-        <input
-          id="lastName"
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
-      </div>
-
-      <div className="form-field">
-        <label htmlFor="meal">Meal Option<span className="required">*</span></label>
-        <select
-          id="meal"
-          name="meal"
-          value={formData.meal}
-          onChange={handleChange}
-          required
-        >
-          <option value="">Select a meal</option>
-          {mealOptions.map((meal) => (
-            <option key={meal} value={meal}>
-              {meal}
-            </option>
-          ))}
-        </select>
+        {hasPlusOne && (
+          <div className="form-fields">
+            {getFormFields(plusOne, (e) => handleChange(e, "plusOne"))}
+          </div>
+        )}
       </div>
 
       <button type="submit">Submit</button>
